f88961a9b7e036656dc78c99a265b3f711466683
German Nunez <german90nunez@gmail.com>
2020-02-18 18:26:00 -0800

    created main components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-    
+    <div >
+      <AddTodo />
+      <TodoList />
+    </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList'
+import AddTodo from './components/AddTodo'
 export default {
+  components : {
+    TodoList,
+    AddTodo
+  },
   data() {
     return {
+      todos: [
+        {
+
+        }
+      ]
     }
   },
- 
 }
 </script>
 
